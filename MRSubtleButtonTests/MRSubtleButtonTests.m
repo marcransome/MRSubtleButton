d273f66d6216ecc86653e7d2186397aa4b10f4e1
Marc Ransome <marc.ransome@fidgetbox.co.uk>
2013-11-15 23:03:50 +0000

    adding basic sample project

---
+++ MRSubtleButtonTests/MRSubtleButtonTests.m
@@ -0,0 +1,34 @@
+//
+//  MRSubtleButtonTests.m
+//  MRSubtleButtonTests
+//
+//  Created by Marc Ransome on 15/11/2013.
+//  Copyright (c) 2013 Marc Ransome. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MRSubtleButtonTests : XCTestCase
+
+@end
+
+@implementation MRSubtleButtonTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
